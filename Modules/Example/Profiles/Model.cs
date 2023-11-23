384f3065ef79d82427bf477b3a2ece68d84e9f1d
Vicham <Vicham@gmail.com>
2023-11-23 10:53:10 +0700

    git init

--- Modules/Example/Profiles/Model.cs
+++ Modules/Example/Profiles/Model.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApi.Core;
 
 namespace WebApi.Modules.Profiles;
 
-public class ProfilesModel : Model
+public class ProfilesModel : Entity
 {
     public Guid FirstName { get; set; }
     public Guid LastName { get; set; }
